f9c9c2f089bda770a383f49191808eab83a55873
Gheorghe <madangheorghe.30@gmail.com>
2025-03-25 17:36:59 +0100

    Remove SearchBar component; update NavBar to include input and button for search functionality; modify Header styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,30 @@
 import '../style/HeaderStyle.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import SearchBar from './SearchBar';
 export default function NavBar() {
+    // setto lo stato del componente EditMovieForm a false per non renderizzarlo all'avvio della pagina
+    const [isOpen, setIsOpen] = useState(false)
+
+    // funzione per aprire il componente EditMovieForm
+    function openEditForm() {
+        setIsOpen(true)
+    }
+    // funzione per chiudere il componente EditMovieForm
+    function closeEditForm() {
+        setIsOpen(false)
+    }
+
     return (
         <>
-            <div>
+            <div className='container-nav'>
                 <div>
-                    <FontAwesomeIcon icon={faBars} />
-                </div>
-                <div>
+                    <FontAwesomeIcon icon={faBars} className='burger-menu' />
                     logo
                 </div>
                 <div>
-                    <SearchBar />
+                    <input type="text" />
+                    <button>Cerca</button>
                 </div>
-
             </div>
         </>
     )
--- src/components/SearchBar.jsx
+++
@@ -1,7 +0,0 @@
-export default function SearchBar() {
-    return (
-        <>
-            <input type="text" placeholder="fai la tua ricerca" className="search-input" />
-        </>
-    )
-}
