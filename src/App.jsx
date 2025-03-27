654fc8291680b2a4f7047a9af8cb33c65d057940
Mirko <mirkocaccavone03@gmail.com>
2025-03-27 17:25:07 +0100

    Fix routing by removing duplicate routes and ensuring ProductPage is included

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,8 @@ import XboxPage from './pages/XboxPage'
 import PlaystationPage from './pages/PlaystationPage'
 import NintendoPage from './pages/NintendoPage'
 import CartPage from './pages/CartPage'
-<<<<<<< HEAD
 import CheckoutPage from './pages/CheckoutPage'
-=======
 import ProductPage from './pages/ProductPage'
->>>>>>> e3f51e356e05e2300b8ac3387ebb9c473302535f
 
 // import del GlobalContext
 import { GlobalProvider } from './contexts/GlobalContext'
@@ -33,7 +30,6 @@ function App() {
       {/* Avvolgiamo l'intera app con il GlobalProvider */}
       <GlobalProvider>
         <BrowserRouter>
-<<<<<<< HEAD
           <Elements stripe={stripePromise}>
             <Routes>
               <Route element={<DefaultLayout />}>
@@ -42,22 +38,11 @@ function App() {
                 <Route path='/xbox' element={<XboxPage />} />
                 <Route path='/nintendo' element={<NintendoPage />} />
                 <Route path='/cart' element={<CartPage />} />
+                <Route path='/product' element={<ProductPage />} />
                 <Route path='/checkout' element={<CheckoutPage stripePromise={stripePromise} />} />
               </Route>
             </Routes>
           </Elements>
-=======
-          <Routes>
-            <Route element={<DefaultLayout />}>
-              <Route index element={<HomePage />} />
-              <Route path='/playstation' element={<PLaystationPage />} />
-              <Route path='/xbox' element={<XboxPage />} />
-              <Route path='/nintendo' element={<NintendoPage />} />
-              <Route path='/cart' element={<CartPage />} />
-              <Route path='/product' element={<ProductPage />} />
-            </Route>
-          </Routes>
->>>>>>> e3f51e356e05e2300b8ac3387ebb9c473302535f
         </BrowserRouter>
       </GlobalProvider>
     </>
