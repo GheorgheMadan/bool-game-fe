9520b8caa5fa8fb2b4e035f4e6512276e7b6ea57
Mirko <mirkocaccavone03@gmail.com>
2025-03-27 18:14:18 +0100

    Integrate Stripe for payment processing and update context provider in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-// import delle librerire necessarie per le rotte del app
+// import delle librerie necessarie per le rotte dell'app
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// import di Stripe
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js'
 
 // import del layout di default
 import DefaultLayout from './Layouts/DefaultLayout'
@@ -10,33 +14,37 @@ import XboxPage from './pages/XboxPage'
 import PlaystationPage from './pages/PlaystationPage'
 import NintendoPage from './pages/NintendoPage'
 import CartPage from './pages/CartPage'
+import CheckoutPage from './pages/CheckoutPage'
 import ProductPage from './pages/ProductPage'
 
 // import del GlobalContext
-import GlobalContext from './contexts/GlobalContext'
+import { GlobalProvider } from './contexts/GlobalContext'
 
-
+// carica Stripe con la chiave pubblica
+const stripePromise = loadStripe('pk_test_51R4oHvPXYjIqouRcxeNvWgRuFnQ1vo8PlqPEzVS6mDT2ix4nlzdDPaqwSVD5oHDfiTx0xdfcL0IzUQTsy4IU1bvA0079IF49Us');
 
 function App() {
-
   return (
     <>
-      <GlobalContext.Provider>
+      {/* Avvolgiamo l'intera app con il GlobalProvider */}
+      <GlobalProvider>
         <BrowserRouter>
-          <Routes>
-            <Route element={<DefaultLayout />}>
-              <Route index element={<HomePage />} />
-              <Route path='/playstation' element={<PlaystationPage />} />
-              <Route path='/xbox' element={<XboxPage />} />
-              <Route path='/nintendo' element={<NintendoPage />} />
-              <Route path='/cart' element={<CartPage />} />
-              <Route path='/product' element={<ProductPage />} />
-            </Route>
-          </Routes>
+          <Elements stripe={stripePromise}>
+            <Routes>
+              <Route element={<DefaultLayout />}>
+                <Route index element={<HomePage />} />
+                <Route path='/playstation' element={<PlaystationPage />} />
+                <Route path='/xbox' element={<XboxPage />} />
+                <Route path='/nintendo' element={<NintendoPage />} />
+                <Route path='/cart' element={<CartPage />} />
+                <Route path='/product' element={<ProductPage />} />
+                <Route path='/checkout' element={<CheckoutPage stripePromise={stripePromise} />} />
+              </Route>
+            </Routes>
+          </Elements>
         </BrowserRouter>
-      </GlobalContext.Provider>
+      </GlobalProvider>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
