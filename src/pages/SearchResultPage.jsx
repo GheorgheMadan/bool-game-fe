b61b2ac59aaf7dffa8582279edd09a6707bd0146
Gheorghe <madangheorghe.30@gmail.com>
2025-03-27 21:01:32 +0100

    feat: enhance search results page layout with new styling

--- src/pages/SearchResultPage.jsx
+++ src/pages/SearchResultPage.jsx
@@ -2,12 +2,13 @@
 // import { useLocation } from 'react-router-dom';
 import { useContext } from 'react';
 import GlobalContextResults from '../contexts/GlobalContextResult';
+import '../style/SearchResultsStyle.css'
 export default function SearchResults() {
     const { results } = useContext(GlobalContextResults)
     console.log(results);
 
     return (
-        <div className='home-container'>
+        <div className='container-search-page'>
             <h2>Risultati della ricerca</h2>
             <div className='container'>
                 {results.map(result => {
