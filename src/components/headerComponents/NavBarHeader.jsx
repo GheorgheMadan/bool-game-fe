cd10aac3f85d60c09839bc45663b9e428d9ae3ec
Gheorghe <madangheorghe.30@gmail.com>
2025-03-31 19:04:23 +0200

    [feat] Add new jumbotron images for Xbox, PlayStation, and Nintendo; enhance NavBar with mobile menu toggle

--- src/components/headerComponents/NavBarHeader.jsx
+++ src/components/headerComponents/NavBarHeader.jsx
@@ -5,24 +5,62 @@ import { faPlaystation, faXbox } from '@fortawesome/free-brands-svg-icons';
 import { NavLink } from 'react-router';
 import { BsNintendoSwitch } from "react-icons/bs";
 import SearchBar from './SearchBar';
+import { Link } from 'react-router';
+import { FaBars } from "react-icons/fa6";
+import { useState } from 'react';
+
 
 export default function NavBarHeader() {
+    const [isOpen, setIsOpen] = useState(false)
 
+    function toggleMenu() {
+        setIsOpen(!isOpen)
+    }
 
     return (
         <>
             <div className='container-nav'>
-                <div className='logo'>Next Level Shop</div>
+                <div className='logo'>
+                    <FaBars className='burger-icon' onClick={toggleMenu} />
+                    <Link to='/'>Next Level Shop</Link>
+                </div>
                 <div className='container-link'>
-                    <NavLink to='/' className='icon home'><FontAwesomeIcon icon={faHome} /></NavLink>
-                    <NavLink to='/playstation' className='icon playstation'><FontAwesomeIcon icon={faPlaystation} /></NavLink>
-                    <NavLink to='/xbox' className='icon xbox'><FontAwesomeIcon icon={faXbox} /></NavLink>
+                    <NavLink to='/' className='icon home' >
+                        <FontAwesomeIcon icon={faHome} /></NavLink>
+                    <NavLink to='/playstation' className='icon playstation'>
+                        <FontAwesomeIcon icon={faPlaystation} />
+                    </NavLink>
+                    <NavLink to='/xbox' className='icon xbox'>
+                        <FontAwesomeIcon icon={faXbox} />
+                    </NavLink>
                     <NavLink to='/nintendo' className='icon nintendo'>
                         <BsNintendoSwitch />
                     </NavLink>
                 </div>
                 <SearchBar />
             </div>
+            {
+                isOpen &&
+                <div className={`container-b-menu ${isOpen ? 'open' : 'close'}`} onClick={toggleMenu}>
+                    <NavLink to='/' className='icon home' onClick={() => setIsOpen(false)}>
+                        <FontAwesomeIcon icon={faHome} />
+                        <span>Home</span>
+                    </NavLink>
+                    <NavLink to='/playstation' className='icon playstation' onClick={() => setIsOpen(false)}>
+                        <FontAwesomeIcon icon={faPlaystation} />
+                        <span>PlayStation</span>
+                    </NavLink>
+                    <NavLink to='/xbox' className='icon xbox' onClick={() => setIsOpen(false)}>
+                        <FontAwesomeIcon icon={faXbox} />
+                        <span>Xbox</span>
+                    </NavLink>
+                    <NavLink to='/nintendo' className='icon nintendo' onClick={() => setIsOpen(false)}  >
+                        <BsNintendoSwitch />
+                        <span>Nintendo</span>
+                    </NavLink>
+                </div>
+            }
+
         </>
     )
 }
