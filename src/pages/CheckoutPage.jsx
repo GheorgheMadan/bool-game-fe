b06b70cb61c132aa7b4432950fea93954aa0e3c4
Mirko <mirkocaccavone03@gmail.com>
2025-04-01 21:29:07 +0200

    [fix] modify media

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -167,7 +167,7 @@ const CheckoutForm = () => {
   };
 
   return (
-    <div className="container">
+    <div className="checkout-container">
       {/* CHECKOUT */}
       <div className="checkout">
         <h1>Checkout</h1>
@@ -187,17 +187,9 @@ const CheckoutForm = () => {
         </h2>
       </div>
 
-      {/* <div className="centered-price">
-        Spedizione gratuita per ordini superiori a 40€
-      </div>
-
-      <div className="centered-price">
-        Costo spedizione: €{shippingCost}{" "}
-        {shippingCost === "0.00" && "(gratis)"}
-      </div> */}
-
+      {/* Campi per i dettagli dell'utente */}
       <form>
-        {/* Campi per i dettagli dell'utente */}
+
 
         <div className="personal_data">
           <div className="input_group">
